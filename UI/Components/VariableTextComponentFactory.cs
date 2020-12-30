d43703df6f4f8b8ff635b82ba0d22cc0e812dc4e
Charles Madere <charlesrmadere@gmail.com>
2020-12-30 16:55:53 +0900

    initial commit
    currently pretty much just a clone of the LiveSplit.Text repository

---
+++ UI/Components/VariableTextComponentFactory.cs
@@ -0,0 +1,24 @@
+﻿using LiveSplit.Model;
+using System;
+
+namespace LiveSplit.UI.Components
+{
+    public class VariableTextComponentFactory : IComponentFactory
+    {
+        public string ComponentName => "Variable Text";
+
+        public string Description => "Displays any text that you want it to show, with support for simple variable expansion.";
+
+        public ComponentCategory Category => ComponentCategory.Information;
+
+        public IComponent Create(LiveSplitState state) => new VariableTextComponent(state);
+
+        public string UpdateName => ComponentName;
+
+        public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.VariableText.xml";
+
+        public string UpdateURL => "http://livesplit.org/update/";
+
+        public Version Version => Version.Parse("0.0.1");
+    }
+}
---
+++ UI/Components/VariableTextTextComponent.cs
@@ -0,0 +1,19 @@
+﻿namespace LiveSplit.UI.Components
+{
+    public class VariableTextTextComponent : InfoTextComponent
+    {
+        public VariableTextComponentSettings Settings { get; set; }
+
+        public VariableTextTextComponent(VariableTextComponentSettings settings) : base("", "")
+        {
+            Settings = settings;
+        }
+
+        public override void PrepareDraw(Model.LiveSplitState state, LayoutMode mode)
+        {
+            NameMeasureLabel.Font = Settings.OverrideFont1 ? Settings.Font1 : state.LayoutSettings.TextFont;
+            ValueLabel.Font = Settings.OverrideFont2 ? Settings.Font2 : state.LayoutSettings.TextFont;
+            NameLabel.Font = Settings.OverrideFont1 ? Settings.Font1 : state.LayoutSettings.TextFont;
+        }
+    }
+}
